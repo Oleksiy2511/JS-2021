aa90e6f54ecc706595713a30511d1de21ab94a29
USERoleksiy#87741 <kurulenko275@gmail.com>
2021-12-09 00:42:13 +0200

    errors 1

--- JSJSJSJS/js.js
+++ JSJSJSJS/js.js
@@ -1,76 +1,21 @@
 
-// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
-// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
-//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
-// alert('готовий до роботи?');
-// confirm('ти ментор?');
-// let firstName = 'Oleksiy';
-// let middleName = 'Oleksiyovich';
-// let lastName = 'Kyrylenko';
-// let year = 1234567890;
-// let person = [lastName+firstName+middleName+year];
 
+// Ваше завдання, зробити так, щоб файл відкривався без помилок .
 
-// let info={
-//     name: 'Oleksiy',
-//     lastName: 'Kyrylenko',
-//     age: 38,
-//     country: 'Ukrain',
-//     city:'Lviv',
-//     home:['Balaton','Gorodnitska','Hmelnitskogo','Bruhy'],
-//     provinciya:{
-//         name:'Bruhovichi',
-//         streat:'Vereskova',
-//         buid:14
-//     }}
-// console.log(info.age,)
-// console.log(info.name,)
-// console.log(info['provinciya']["name"])
-// let p=prompt('enter key');
-// console.log(p);
-// console.log(info[p]);
-// document.write(info[p]);
-// console.log(info["home"][3])
-// console.log(info.provinciya.name)
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+const x = 'constant value';
+{
+    console.log(a);
+    a = 'new value';
+}
+let y = 'new value';
 
+let result = 100 + y;
 
-
-
-// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
-// Значення площі зберігати в змінній s.
-// let height=23;
-// let width=10;
-// let s=height*width;
-// console.log(s)
-
-
-//об'єм
-// let p =+prompt('висота');
-// let p1=+prompt('довжина');
-// let p2=+prompt('ширина');
-// console.log(p*p2)
-// alert(p*p1*p2)
-
-
-// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 5м (dC),
-// результат помістіть у змінну v.
-// pi*(R**2)*h
-// hC=10m,  dC=4m,  RC=dC/2
-//3.1415*((5/2)**2)*10=3.1415*6.25*10=*10=628
-// let hC = +prompt('height');
-// let dC = +prompt('diameter');
-// let rC=dC/2;
-// let pi=3.1415
-// let v=pi*(rC**2)*hC;
-// alert(pi*(rC**2)*hC);
-// console.log(v)
-
-
-
-
-// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
-// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
-//
+console.log(result);
 
 
 
